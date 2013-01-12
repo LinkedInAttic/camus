26a7fa7b14624c6f5f0f81676a489eb7f03e42a3
kgoodhop <kgoodhope@linkedin.com>
2013-01-11 23:40:45 -0800

    adding schema implementation for the first schema patch in avro

---
+++ src/main/java/com/linkedin/batch/etl/kafka/schemaregistry/AvroJdbcSchemaRegistryClient.java
@@ -0,0 +1,70 @@
+package com.linkedin.batch.etl.kafka.schemaregistry;
+
+import org.apache.avro.repository.IsAvroValidator;
+import org.apache.avro.repository.JdbcSchemaRepository;
+import org.apache.avro.repository.Md5SchemaIdGenerator;
+import org.apache.avro.repository.SchemaAndId;
+import org.apache.avro.repository.SchemaChangeValidator;
+import org.apache.avro.repository.SchemaIdGenerator;
+import org.apache.avro.repository.SchemaRepository;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.JobContext;
+
+import com.linkedin.batch.etl.kafka.EtlJob;
+import com.linkedin.batch.etl.kafka.mapred.EtlInputFormat;
+
+/**
+ * Wrapper around the Schema Repository Interface
+ * 
+ * @author
+ * 
+ */
+public class AvroJdbcSchemaRegistryClient implements SchemaRegistry
+{
+
+	private JdbcSchemaRepository client;
+	
+  public AvroJdbcSchemaRegistryClient(Configuration conf) throws InstantiationException, IllegalAccessException, ClassNotFoundException
+  {
+	  SchemaChangeValidator validator = new IsAvroValidator();
+      SchemaIdGenerator idGenerator = new Md5SchemaIdGenerator();
+    
+      client =
+          new JdbcSchemaRepository(validator,
+                                   idGenerator,
+                                   conf.get("schema.registry.user"),
+                                   conf.get("schema.registry.password"),
+                                   conf.get(EtlJob.ETL_SCHEMA_REGISTRY_URL),
+                                   conf.get(EtlJob.JDBC_SCHEMA_REGISTRY_DRIVER),
+                                   conf.getInt(EtlJob.JDBC_SCHEMA_REGISTRY_POOL_SIZE, 8));
+  }
+
+@Override
+public String register(String topic, String schema)
+		throws SchemaRegistryException {
+	return client.register(topic, schema);
+}
+
+@Override
+public String getSchemaByID(String topic, String id)
+		throws SchemaNotFoundException {
+	String schema = client.lookup(topic, id);
+	
+	if (schema != null)
+		return schema;
+	else
+		throw new SchemaNotFoundException("Schema not found for " + topic + " " + id);
+}
+
+@Override
+public SchemaDetails getLatestSchemaByTopic(String topicName) throws SchemaNotFoundException {
+	SchemaAndId schema = client.lookupLatest(topicName);
+	
+	if (schema != null)
+		return new SchemaDetails(schema.getId(), schema.getSchema(), topicName);
+	else
+		throw new SchemaNotFoundException("Schema not found for " + topicName);
+	
+}
+
+}

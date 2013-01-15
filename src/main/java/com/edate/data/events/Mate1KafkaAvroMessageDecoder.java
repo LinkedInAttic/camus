bf606f5061869e02f87bba82044d4dcdfce2f836
Felix GV <felix@mate1inc.com>
2013-01-14 21:41:45 -0500

    Concrete implementation of a Mate1-specific KafkaMessageDecoder, which uses an enum we have that is capable of mapping a kafka topic to its corresponding avro schema. This enum acts as some sort of unversioned schema registry.

---
+++ src/main/java/com/edate/data/events/Mate1KafkaAvroMessageDecoder.java
@@ -0,0 +1,64 @@
+package com.edate.data.events;
+
+import java.io.IOException;
+
+import kafka.message.Message;
+
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.hadoop.conf.Configuration;
+
+import com.linkedin.batch.etl.kafka.coders.KafkaMessageDecoder;
+import com.linkedin.batch.etl.kafka.schemaregistry.CachedSchemaResolver;
+
+import com.edate.data.events.EventDataAvro.EventTypeAvro;
+
+public class Mate1KafkaAvroMessageDecoder extends KafkaMessageDecoder
+{
+
+	private final DecoderFactory decoderFactory;
+
+	public Mate1KafkaAvroMessageDecoder(Configuration conf)
+	{
+		super(conf);
+		this.decoderFactory = new DecoderFactory();
+	}
+
+	@Override
+	public Record toRecord(String topicName, Message message, BinaryDecoder decoderReuse)
+	{
+		EventTypeAvro type = EventDataAvro.EventTypeAvro.valueOf(topicName);
+
+		try
+		{
+			DatumReader<Record> reader = new GenericDatumReader<Record>(type.getSchema());
+
+			return reader.read(null, decoderFactory.binaryDecoder(message.payload().array(), decoderReuse));
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public <T extends SpecificRecord> T toSpecificRecord(String topicName, Message message, BinaryDecoder decoderReuse)
+	{
+		EventTypeAvro type = EventDataAvro.EventTypeAvro.valueOf(topicName);
+
+		try
+		{
+			DatumReader<T> reader = new GenericDatumReader<T>(type.getSchema());
+
+			return reader.read(null, decoderFactory.binaryDecoder(message.payload().array(), decoderReuse));
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+}

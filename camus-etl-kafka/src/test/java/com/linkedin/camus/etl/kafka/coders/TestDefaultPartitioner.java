5f7a42c635a53240e20cfcb953fd4459d8a67b9d
Andrew Ehrlich <ehrlichja@gmail.com>
2014-07-30 20:46:57 -0700

    using configuration object

--- camus-etl-kafka/src/test/java/com/linkedin/camus/etl/kafka/coders/TestDefaultPartitioner.java
+++ camus-etl-kafka/src/test/java/com/linkedin/camus/etl/kafka/coders/TestDefaultPartitioner.java
@@ -24,9 +24,10 @@ public class TestDefaultPartitioner {
         JobContext testJobContext = new JobContext(testConfiguration, testJobID);
 
         DefaultPartitioner testPartitioner = new DefaultPartitioner();
+        testPartitioner.setConf(testConfiguration);
 
         String actualResult = testPartitioner.generatePartitionedPath(testJobContext, "testTopic", "testBrokerId", 123, "1406777693000");
-        String expectedResult = "testTopic/hourly/2014-07-30T20:34:53.000-07:00";
+        String expectedResult = "testTopic/hourly/2014/07/30/20";
 
         assertTrue(actualResult.equals(expectedResult));
     }

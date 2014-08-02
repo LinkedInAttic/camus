ea40500a33062ca1f93d1b7bd2b88a9a8a9bb657
Andrew Ehrlich <ehrlichja@gmail.com>
2014-08-01 20:01:50 -0700

    new unit tests for JsonStringMessageDecoder. cleanup.

--- camus-etl-kafka/src/test/java/com/linkedin/camus/etl/kafka/coders/TestDefaultPartitioner.java
+++ camus-etl-kafka/src/test/java/com/linkedin/camus/etl/kafka/coders/TestDefaultPartitioner.java
@@ -1,18 +1,10 @@
 package com.linkedin.camus.etl.kafka.coders;
 
-/**
- * Created by ehrlichja on 7/30/14.
- */
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.JobID;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-
 
 public class TestDefaultPartitioner {
 

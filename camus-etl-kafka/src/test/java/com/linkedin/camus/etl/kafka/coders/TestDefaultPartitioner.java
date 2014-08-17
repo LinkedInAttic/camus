fffa0a8938f8696807d570ab61b87d2c882e45ce
Benedikt Böhm <bb@xnull.de>
2014-08-17 13:52:58 +0200

    upgrade to hadoop 2.4.1

--- camus-etl-kafka/src/test/java/com/linkedin/camus/etl/kafka/coders/TestDefaultPartitioner.java
+++ camus-etl-kafka/src/test/java/com/linkedin/camus/etl/kafka/coders/TestDefaultPartitioner.java
@@ -2,6 +2,7 @@ package com.linkedin.camus.etl.kafka.coders;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.task.JobContextImpl;
 import org.apache.hadoop.mapreduce.JobID;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
@@ -13,7 +14,7 @@ public class TestDefaultPartitioner {
         // generatePartitionPath() should take a timestamp and return a formatted string by default
         Configuration testConfiguration = new Configuration();
         JobID testJobID = new JobID();
-        JobContext testJobContext = new JobContext(testConfiguration, testJobID);
+        JobContext testJobContext = new JobContextImpl(testConfiguration, testJobID);
 
         DefaultPartitioner testPartitioner = new DefaultPartitioner();
         testPartitioner.setConf(testConfiguration);

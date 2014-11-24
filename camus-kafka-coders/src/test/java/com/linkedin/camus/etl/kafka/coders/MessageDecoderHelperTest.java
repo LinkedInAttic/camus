bd33efbe97daa069c223915e383c92d66a69f625
Andrew Ehrlich <ehrlichja@gmail.com>
2014-11-23 18:53:58 -0800

    more unit tests

--- camus-kafka-coders/src/test/java/com/linkedin/camus/etl/kafka/coders/MessageDecoderHelperTest.java
+++ camus-kafka-coders/src/test/java/com/linkedin/camus/etl/kafka/coders/MessageDecoderHelperTest.java
@@ -4,6 +4,9 @@ import com.linkedin.camus.schemaregistry.SchemaRegistry;
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
 import org.junit.Test;
+
+import java.nio.BufferUnderflowException;
+
 import static org.junit.Assert.assertEquals;
 import static org.easymock.EasyMock.*;
 
@@ -32,28 +35,39 @@ public class MessageDecoderHelperTest {
 
     }
 
-    @Test
-    public void testNoBody() {
+    @Test(expected = BufferUnderflowException.class)
+    public void testNoIdNoBody() {
 
         SchemaRegistry schemaRegistry = createMock(SchemaRegistry.class);
-        Schema schema = SchemaBuilder.record("testRecord").namespace("my.name.space")
-                .fields().name("field").type().stringType().noDefault().endRecord();
         replay(schemaRegistry);
 
         KafkaAvroMessageDecoder kafkaAvroMessageDecoder = new KafkaAvroMessageDecoder();
         final byte[] bytes = new byte[1];
         bytes[0] = 0x0; // Magic byte
         KafkaAvroMessageDecoder.MessageDecoderHelper messageDecoderHelper = kafkaAvroMessageDecoder.new MessageDecoderHelper(schemaRegistry, "myTopic", bytes);
+        messageDecoderHelper.invoke();
         verify(schemaRegistry);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testWithNoMagicByte() {
 
-        SchemaRegistry s = createMock(SchemaRegistry.class);
+        SchemaRegistry schemaRegistry = createMock(SchemaRegistry.class);
 
         KafkaAvroMessageDecoder kafkaAvroMessageDecoder = new KafkaAvroMessageDecoder();
-        KafkaAvroMessageDecoder.MessageDecoderHelper messageDecoderHelper = kafkaAvroMessageDecoder.new MessageDecoderHelper(s, "myTopic", "whatever".getBytes());
+        KafkaAvroMessageDecoder.MessageDecoderHelper messageDecoderHelper = kafkaAvroMessageDecoder.new MessageDecoderHelper(schemaRegistry, "myTopic", "whatever".getBytes());
+        messageDecoderHelper.invoke();
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testUnknownSchemaId() {
+        SchemaRegistry schemaRegistry = createMock(SchemaRegistry.class);
+        final byte[] bytes = "whatever".getBytes();
+        bytes[0] = 0x0; // Magic byte
+
+        KafkaAvroMessageDecoder kafkaAvroMessageDecoder = new KafkaAvroMessageDecoder();
+        KafkaAvroMessageDecoder.MessageDecoderHelper messageDecoderHelper = kafkaAvroMessageDecoder.new MessageDecoderHelper(schemaRegistry, "myTopic", bytes);
         messageDecoderHelper.invoke();
 
     }

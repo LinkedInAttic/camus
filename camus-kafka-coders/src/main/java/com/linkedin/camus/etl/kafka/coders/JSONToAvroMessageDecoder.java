20b334666539fe9f86645fb0f7799e923bfc2504
Santosh Pingale <santosh.pingale@makemytrip.com>
2015-03-29 13:53:04 +0530

    Added Type for generic. changed indentation.

--- camus-kafka-coders/src/main/java/com/linkedin/camus/etl/kafka/coders/JSONToAvroMessageDecoder.java
+++ camus-kafka-coders/src/main/java/com/linkedin/camus/etl/kafka/coders/JSONToAvroMessageDecoder.java
@@ -32,60 +32,69 @@ import com.linkedin.camus.schemaregistry.CachedSchemaRegistry;
 import com.linkedin.camus.schemaregistry.SchemaRegistry;
 
 public class JSONToAvroMessageDecoder extends MessageDecoder<byte[], GenericData.Record> {
-  private static final Logger      log                            = Logger.getLogger(JsonStringMessageDecoder.class);
-  public static final String       CAMUS_SCHEMA_ID_FIELD          = "camus.message.schema.id.field";
-  public static final String       DEFAULT_SCHEMA_ID_FIELD        = "schemaID";
-  JsonParser                       jsonParser;
-  private String                   schemaIDField;
-  protected DecoderFactory         decoderFactory;
+  private static final Logger log = Logger.getLogger(JsonStringMessageDecoder.class);
+  public static final String CAMUS_SCHEMA_ID_FIELD = "camus.message.schema.id.field";
+  public static final String DEFAULT_SCHEMA_ID_FIELD = "schemaID";
+  JsonParser jsonParser;
+  private String schemaIDField;
+  protected DecoderFactory decoderFactory;
   protected SchemaRegistry<Schema> registry;
-  private Schema                   latestSchema;
-
+  private Schema latestSchema;
+  
   public JSONToAvroMessageDecoder() {
     this.jsonParser = new JsonParser();
   }
-
+  
   public void init(Properties props, String topicName) {
     super.init(props, topicName);
     this.props = props;
     this.topicName = topicName;
-
+    
     this.schemaIDField = props.getProperty(CAMUS_SCHEMA_ID_FIELD, DEFAULT_SCHEMA_ID_FIELD);
     try {
-      SchemaRegistry<Schema> registry = (SchemaRegistry<Schema>) Class.forName(props.getProperty(KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS)).newInstance();
-      log.info("Prop " + KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS + " is: " + props.getProperty(KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS));
+      SchemaRegistry<Schema> registry = (SchemaRegistry<Schema>) Class.forName(
+          props.getProperty(KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS)).newInstance();
+      log.info("Prop " + KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS + " is: "
+          + props.getProperty(KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS));
       log.info("Underlying schema registry for topic: " + topicName + " is: " + registry);
       registry.init(props);
-
+      
       this.registry = new CachedSchemaRegistry<Schema>(registry, props);
       this.latestSchema = ((Schema) registry.getLatestSchemaByTopic(topicName).getSchema());
     } catch (Exception e) {
       throw new MessageDecoderException(e);
     }
-
+    
     this.decoderFactory = DecoderFactory.get();
   }
-
+  
   public CamusWrapper<GenericData.Record> decode(byte[] payload) {
     String payloadString = new String(payload);
     JsonObject jsonObject;
     try {
       jsonObject = this.jsonParser.parse(payloadString.trim()).getAsJsonObject();
       String templateID = jsonObject.get(schemaIDField).getAsString();
+      
       MessageDecoderHelper helper = new MessageDecoderHelper(this.registry, this.topicName).invoke(templateID);
       GenericRecord datum = null;
-      DatumReader reader = helper.getTargetSchema() == null ? new GenericDatumReader(helper.getSchema()) : new GenericDatumReader(helper.getSchema(), helper.getTargetSchema());
+      DatumReader<GenericRecord> reader = helper.getTargetSchema() == null ? new GenericDatumReader<GenericRecord>(
+          helper.getSchema()) : new GenericDatumReader<GenericRecord>(helper.getSchema(), helper.getTargetSchema());
       InputStream inStream = new ByteArrayInputStream(payload);
       JsonDecoder jsonDecoder = DecoderFactory.get().jsonDecoder(helper.getSchema(), inStream);
       datum = (GenericRecord) reader.read(datum, jsonDecoder);
       ByteArrayOutputStream output = new ByteArrayOutputStream();
-      GenericDatumWriter writer = helper.getTargetSchema() == null ? new GenericDatumWriter(helper.getSchema()) : new GenericDatumWriter(helper.getTargetSchema());
+      GenericDatumWriter<GenericRecord> writer = helper.getTargetSchema() == null ? new GenericDatumWriter<GenericRecord>(
+          helper.getSchema()) : new GenericDatumWriter<GenericRecord>(helper.getTargetSchema());
       Encoder encoder = EncoderFactory.get().binaryEncoder(output, null);
+      
       writer.write(datum, encoder);
       encoder.flush();
       output.close();
-      DatumReader avroReader = helper.getTargetSchema() == null ? new GenericDatumReader(helper.getSchema()) : new GenericDatumReader(helper.getTargetSchema());
-      return new KafkaAvroMessageDecoder.CamusAvroWrapper((GenericData.Record) avroReader.read(null, this.decoderFactory.binaryDecoder(output.toByteArray(), 0, output.toByteArray().length, null)));
+      
+      DatumReader<GenericRecord> avroReader = helper.getTargetSchema() == null ? new GenericDatumReader<GenericRecord>(
+          helper.getSchema()) : new GenericDatumReader<GenericRecord>(helper.getTargetSchema());
+      return new KafkaAvroMessageDecoder.CamusAvroWrapper((GenericData.Record) avroReader.read(null,
+          this.decoderFactory.binaryDecoder(output.toByteArray(), 0, output.toByteArray().length, null)));
     } catch (RuntimeException e) {
       log.error("Caught exception while parsing JSON string '" + payloadString + "'.");
       throw new RuntimeException(e);
@@ -93,26 +102,26 @@ public class JSONToAvroMessageDecoder extends MessageDecoder<byte[], GenericData
       throw new MessageDecoderException(e);
     }
   }
-
+  
   public class MessageDecoderHelper {
-    private Schema                       schema;
-    private Schema                       targetSchema;
+    private Schema schema;
+    private Schema targetSchema;
     private final SchemaRegistry<Schema> registry;
-    private final String                 topicName;
-
+    private final String topicName;
+    
     public MessageDecoderHelper(SchemaRegistry<Schema> registry, String topicName) {
       this.registry = registry;
       this.topicName = topicName;
     }
-
+    
     public Schema getSchema() {
       return this.schema;
     }
-
+    
     public Schema getTargetSchema() {
       return this.targetSchema;
     }
-
+    
     public MessageDecoderHelper invoke(String id) {
       this.schema = (this.registry.getSchemaByID(this.topicName, id));
       if (this.schema == null)
